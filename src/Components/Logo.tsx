693df01cfce864ffe09bc80c47639d9ea5a51fca
wafaes <alumno.554266@ies-azarquiel.es>
2025-02-17 13:44:50 +0100

    prueba2

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,10 +1,6 @@
 function Logo() {
   return (
-<<<<<<< HEAD
-    <img src="/logo_hive.svg" alt="logoHive" className="w-24" />
-=======
     <img src="/hive-logo.png" alt="logoHive" className="w-24" />
->>>>>>> wafae
   )
 }
 
